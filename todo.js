488e6c01291e1d2b061f24fdef023d54ae4646d7
bakerville <thebakeriscomingforu@gmail.com>
2017-06-20 16:31:39 -0500

    Added initial files and starter .js code

---
+++ todo.js
@@ -0,0 +1,15 @@
+/* version 1.0 Arrays*/
+/*1) It should have a place to store todos.*/
+/*var todos =['item1', 'item2', 'item3','item4','item5'];*/
+/*2) It should have a way to display todos.*/
+/*console.log('my todos', todos);*/
+/*3) It should have a way to add new todos.*/
+/*todos.push('new todos');*/
+/* 4) It should have a way to change a todo.*/
+//* 4) Example changing the 1st item in the array.*/
+/*todos[0] ="Changed!";*/
+/* 5) It should have a way to delete a todo.*/
+//* 5) Example deleting the first item by stating its position and how many to delete.*/
+/*todos.splice(0,1);*/
+
+
