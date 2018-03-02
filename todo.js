8ddcd53a187eae345fbce876f419b9aa3c1253b7
rlauderdale <rjlauderdale26@gmail.com>
2018-03-02 07:54:44 -0600

    comments added

--- todo.js
+++ todo.js
@@ -1,15 +1,32 @@
-/* version 1.0 Arrays*/
-/*1) It should have a place to store todos.*/
-/*var todos =['item1', 'item2', 'item3','item4','item5'];*/
-/*2) It should have a way to display todos.*/
-/*console.log('my todos', todos);*/
-/*3) It should have a way to add new todos.*/
-/*todos.push('new todos');*/
-/* 4) It should have a way to change a todo.*/
-//* 4) Example changing the 1st item in the array.*/
-/*todos[0] ="Changed!";*/
-/* 5) It should have a way to delete a todo.*/
-//* 5) Example deleting the first item by stating its position and how many to delete.*/
-/*todos.splice(0,1);*/
+/* version 1.0 Arrays
+1) It should have a place to store todos.
+var todos =['item1', 'item2', 'item3','item4','item5'];
+It should have a way to display todos.
+console.log('my todos', todos);
+3) It should have a way to add new todos.
+todos.push('new todos');
+4) It should have a way to change a todo.
+4) Example changing the 1st item in the array.
+todos[0] ="Changed!";
+5) It should have a way to delete a todo.
+5) Example deleting the first item by stating its position and how many to delete.
+todos.splice(0,1);*/
 
+/*Version 2.0 Functions 
+makeSandwichWith____
+    get one slice of bread.
+    Add ___.
+    Put a slice of bread on top.
 
+function makeSandwichWith(filling) {
+    get one slice of bread;
+    add filling;
+    put a slice of bread on top;
+}   
+makeSandwichWith('ham')
+
+function sayHiTo(person) {
+    console.log('hi', person);
+}
+sayHiTo('gordon')
+*/
