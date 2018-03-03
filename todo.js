04bb5f89d284b68d738173dc77307f2084d57cb8
rlauderdale <rjlauderdale26@gmail.com>
2018-03-02 20:40:38 -0600

    worked on the function videos
    watched the videos on functions

--- todo.js
+++ todo.js
@@ -1,32 +1,61 @@
 /* version 1.0 Arrays
 1) It should have a place to store todos.
-var todos =['item1', 'item2', 'item3','item4','item5'];
-It should have a way to display todos.
-console.log('my todos', todos);
+--var todos =['item1', 'item2', 'item3','item4','item5'];
+2)It should have a way to display todos.
+--console.log('my todos', todos);
 3) It should have a way to add new todos.
-todos.push('new todos');
+--todos.push('new todos');
 4) It should have a way to change a todo.
-4) Example changing the 1st item in the array.
-todos[0] ="Changed!";
+- Example changing the 1st item in the array.
+--todos[0] ="Changed!";
 5) It should have a way to delete a todo.
-5) Example deleting the first item by stating its position and how many to delete.
+- Example deleting the first item by stating its position and how many to delete.
 todos.splice(0,1);*/
 
 /*Version 2.0 Functions 
-makeSandwichWith____
-    get one slice of bread.
-    Add ___.
-    Put a slice of bread on top.
-
-function makeSandwichWith(filling) {
-    get one slice of bread;
-    add filling;
-    put a slice of bread on top;
-}   
-makeSandwichWith('ham')
-
-function sayHiTo(person) {
-    console.log('hi', person);
+var todos =['item 1', 'item 2', 'item 3']
+1)it should have a function to display todos
+function displayTodos() {
+    console.log('My todos:', todos);}
+2)it should have a function to add todos
+function addTodo(todo){ //addTodo('new todo')
+    todos.push(todo);
+    displayTodos();
 }
-sayHiTo('gordon')
+addTodos('working out');
+3)it should have a function to change todos
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+    displayTodos();
+}
+changeTodo(1, cooking);
+4)it should have a function to delete todos
+function deleteTodo(position){
+    todos.splice(position, 1);
+    displayTodos();
+}
+deleteTodos(0); //deletes the first item
+*/
+
+
+
+/*Interlude-Functions and variables, below doesn't work
+var myName ='Gordon';
+
+function sayName() {
+    var secret = 'watchandcode';
+    console.log(myName);
+}
+sayName();
+console.log(secret);
+
+// if you're inside of function, you can look out and see data, 
+but the opposite isn't true. If you are outside, you can't look in.
+*/
+
+/*Objects
+
+
+
+
 */
