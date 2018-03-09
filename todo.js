b00ac39d8a08eda0e86f2b291b1822a5c9d65d69
rlauderdale <rjlauderdale26@gmail.com>
2018-03-08 20:49:16 -0600

    added a for loop in displayTodos
    within displayTodos, added a for loop

--- todo.js
+++ todo.js
@@ -141,5 +141,36 @@ for (var i = 0; i < testArray.length, i++){
 // .displayTodos should show .todoText
 // .displayTodos should tell you if .todos is empty
 // .displayTodos should show .completed
+var todoList = {
+    todos: [],
+    displayTodos: function (){
+        console.log('My Todos:');
+        // .displayTodos should show .todoText
+        for (var i = 0; i < this.todos.length; i++) {
+            console.log(this.todos[i].todoText);
+        }
+    },
+    // todoList.addTodo should add objects
+    addTodo: function(todoText) {  //addTodo('Hi')
+        this.todos.push({
+            todoText: todoText,    // this would be todoText: 'Hi',
+            completed:false
+        });
+        this.displayTodos();
+    },
 
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText; //using dot notation to grab just the todoText property from the object
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function (position) {
+        let todo = this.todos[position];
+        todo.completed = !todo.completed; //if todo.completed is true, then this will flip it to false, and vice versa
+        this.displayTodos();
+    }
+};
 */
