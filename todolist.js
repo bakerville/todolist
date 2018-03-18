f993f0700f56b84da012f89e657eee80baa84e61
rlauderdale <rjlauderdale26@gmail.com>
2018-03-18 15:07:04 -0500

    applied the buttons in html
    created the buttons to toggleAll, and the displayTodos

--- todolist.js
+++ todolist.js
@@ -206,9 +206,15 @@ var todoList = {
 };
 //version 7 additions
 var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
 
 displayTodosButton.addEventListener('click', function() {
     todoList.displayTodos();
 });
+
+toggleAllButton.addEventListener('click', function() {
+    todoList.toggleAll();
+});
+
 
 */
