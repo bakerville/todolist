fd418b8341b63c1f4e96f16cd6226ff42cedf2eb
rlauderdale <rjlauderdale26@gmail.com>
2018-03-21 20:29:48 -0500

    added a file for interlude
    added a file for interlude notes. Will go back and rewatch the videos.

---
+++ interlude.js
@@ -0,0 +1,16 @@
+//for each function
+/*
+var students =['jonathan', 'jenny', 'elliot'];
+
+function logName(name) {
+    console.log(name);
+}
+
+function forEach(myArray, myfunction) {
+    for(var i =0; i < myArray.length; i++) {
+        myFunction(myArray[i]);
+    }
+}
+
+
+*/
